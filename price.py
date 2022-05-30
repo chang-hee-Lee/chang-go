5c40fe31f8dde0dfb39b8138076007b4071df8e9
chang-hee-Lee <103938530+chang-hee-Lee@users.noreply.github.com>
2022-05-30 23:14:24 +0900

    Create price.py

---
+++ price.py
@@ -0,0 +1,49 @@
+# shopping() 함수
+
+def shopping(shop_file):
+  
+  with open("./data/" + shop_file , mode = 'r', encoding = 'utf-8') as f:
+    lines = f.readlines()
+    shop_dict = {}
+    for line in lines:
+      xx = line.strip()
+      if xx == '':
+        continue
+      name, cost = xx.split()
+      if  cost != 'A' and cost != 'B':
+        shop_dict[name] = cost  
+  
+  R_shop = shop_dict.values()
+  D = str(R_shop).replace('원', '')
+  D = D.replace('dict_values', '')
+  D = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', D)
+  D = D.split()
+  
+  return dict(zip(shop_dict.keys(), D))
+      
+  
+print(shopping("shopA.txt"))
+
+# item_price() 함수
+
+def item_price(shop_file, item):
+    
+  with open("./data/" + shop_file , mode = 'r', encoding = 'utf-8') as f:
+    lines = f.readlines()
+    shop_dict = {}
+    for line in lines:
+      xx = line.strip()
+      if xx == '':
+        continue
+      name, cost = xx.split()
+      if cost != 'A':
+        shop_dict[name] = cost 
+
+  R_shop = shop_dict.values()
+  D = str(R_shop).replace('원', '')
+  D = D.replace('dict_values', '')
+  D = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '',D)
+  D = D.split()
+  return dict(zip(shop_dict.keys(), D))[item], '원' 
+
+print(item_price("shopA.txt", '김치'))
